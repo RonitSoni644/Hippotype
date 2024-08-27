ccfe35e86809cf86e2664feea6eeebaba6688f47
RonitSoni644 <ronitsoni505@gmail.com>
2024-08-27 16:21:00 +0530

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,163 @@
+const words = 'in one good real one not school set they state high life consider on and not come what also for set point can want as while with of order child about school thing never hold find order each too between program work end you home place around while place problem end begin interest while public or where see time those increase interest be give end think seem small as both another a child same eye you between way do who into again good fact than under very head become real possible some write know however late each that with because that place nation only for each change form consider we would interest with world so order or run more open that large write turn never over open each over change still old take hold need give by consider line only leave while what set up number part form want against great problem can because head so first this here would course become help year first end want both fact public long word down also long for without new turn against the because write seem line interest call not if line thing what work people way may old consider leave hold want life between most place may if go who need fact such program where which end off child down change to from people high during people find to however into small new general it do that could old for last get another hand much eye great no work and with but good there last think can around use like number never since world need what we around part show new come seem while some and since still small these you general which seem will place come order form how about just also they with state late use both early too lead general seem there point take general seem few out like might under if ask while such interest feel word right again how about system such between late want fact up problem stand new say move a lead small however large public out by eye here over so be way use like say people work for since interest so face order school good not most run problem group run she late other problem real form what just high no man do under would to each too end point give number child through so this large see get form also all those course to work during about he plan still so like down he look down where course at who plan way so since come against he all who at world because while so few last these mean take house who old way large no first too now off would in this course present order home public school back own little about he develop of do over help day house stand present another by few come that down last or use say take would each even govern play around back under some line think she even when from do real problem between long as there school do as mean to all on other good may from might call world thing life turn of he look last problem after get show want need thing old other during be again develop come from consider the now number say life interest to system only group world same state school one problem between for turn run at very against eye must go both still all a as so after play eye little be those should out after which these both much house become both school this he real and may mean time by real number other as feel at end ask plan come turn by all head increase he present increase use stand after see order lead than system here ask in of look point little too without each for both but right we come world much own set we right off long those stand go both but under now must real general then before with much those at no of we only back these person plan from run new as own take early just increase only look open follow get that on system the mean plan man over it possible if most late line would first without real hand say turn point small set at in system however to be home show new again come under because about show face child know person large program how over could thing from out world while nation stand part run have look what many system order some one program you great could write day do he any also where child late face eye run still again on by as call high the must by late little mean never another seem to leave because for day against public long number word about after much need open change also. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur a inventore blanditiis placeat ipsam id qui accusantium aliquam rerum ullam vero rem cum, ex nulla minima cupiditate ipsum similique esse hic. Dolorum accusantium sint, illo, perferendis unde animi minus voluptate laboriosam nemo officiis modi voluptatum iure alias harum suscipit provident dignissimos rem quae facilis vero voluptatem labore sequi ratione quaerat! Nihil necessitatibus praesentium nesciunt sequi soluta ea quae pariatur explicabo voluptatem labore placeat quo laudantium, delectus voluptate. Ipsa maiores ea aliquid labore reiciendis similique, sint expedita vero cupiditate modi ut impedit neque doloremque animi laboriosam. Dolorem, voluptatum. Nisi quibusdam alias laboriosam, cumque, ab iusto accusamus itaque, est autem quasi sequi in quas nobis. Impedit dolore accusantium hic saepe omnis molestias iste sed suscipit vel animi ipsum adipisci amet eius minus temporibus totam ratione, velit quisquam eos sequi aliquid voluptates aut? Quibusdam nobis natus vero ratione iste, voluptatibus impedit animi sed molestias beatae alias nesciunt repellendus repellat numquam consectetur porro similique quos blanditiis quo nemo voluptates quae inventore sint libero. Minima fugit earum id dolorem maiores repellendus. Itaque sint odio beatae dolor iusto asperiores quae dolorem sequi provident aliquid rerum voluptate vel tenetur laboriosam fugit quisquam blanditiis sed, facilis incidunt repellat consequatur doloribus ipsam dolore id! Doloremque ipsam voluptas quis facilis provident, totam fugiat beatae sequi dolor nam sunt, maiores illo cum aperiam blanditiis dolorum exercitationem, animi atque eos temporibus similique rem! Eaque est quo, nostrum atque amet ut, culpa velit ducimus illo, eos et officiis libero! Dolor ut veritatis id voluptatibus cum at error natus eius quam eos maiores perferendis, qui commodi expedita ipsam, blanditiis ex quod, explicabo cupiditate? Voluptatibus ab, sapiente rerum velit eius dolor odio iste repellat, consequatur quidem alias inventore sed debitis provident totam quisquam, corporis delectus molestias. Consectetur tenetur cum ex, iste nulla harum dignissimos placeat eum ea. Nostrum repellendus eum mollitia iure dolorum, saepe dolores laborum, rem at vel veniam distinctio nam dolor hic ex excepturi, repudiandae fugiat deserunt eligendi velit voluptatum. Voluptatem, in doloribus. Earum quae aspernatur aliquid vero ut rem soluta. Veritatis ex officia nam magni est soluta eum pariatur saepe! Ratione necessitatibus nostrum totam nesciunt. Omnis repellendus ducimus enim odio maxime, architecto rerum dolorem nam similique quo quasi porro. Hic pariatur voluptates beatae fuga excepturi est aut blanditiis necessitatibus, perferendis odio. Quos nisi eaque laborum exercitationem fuga ducimus non enim. Repudiandae ab eveniet ratione. Nihil fuga excepturi quas id saepe nam ut alias voluptatum. Quae exercitationem illo possimus earum. Eligendi nemo natus sapiente beatae tempora similique, perspiciatis quisquam quasi corrupti quidem possimus non odio reprehenderit optio doloremque minima blanditiis ratione. Voluptates eaque quasi laudantium hic totam delectus, dignissimos fugit ea amet eum saepe distinctio labore similique ad quam! Aliquid pariatur voluptatum consequuntur iure id dicta odio quod ad quos assumenda dignissimos sint in magni corrupti quae, nam, soluta, magnam quo sed! Repellat, id libero blanditiis delectus quo alias illum quidem voluptas consequatur modi placeat corporis accusamus ut fuga dicta, mollitia ratione recusandae in, nostrum iure et accusantium? Consectetur, in at! Ipsum similique totam dignissimos, nihil a enim odio illum optio assumenda alias, ratione veritatis perferendis ad itaque laboriosam, esse sequi rerum maiores cumque. Omnis in soluta numquam autem ducimus odit cupiditate voluptas nostrum eius odio, fugiat id quasi quisquam vitae reprehenderit hic at deleniti doloremque cumque architecto illo, quo maxime. Rem adipisci iste, illo nulla repellat accusantium nesciunt sequi facere ea repellendus dolore odit! Impedit voluptates, iure a minima optio dignissimos eveniet aperiam est quisquam maxime officiis nulla aliquam incidunt reprehenderit dolores, nobis aliquid autem rerum laboriosam. Blanditiis sint dolore, tempore nihil perferendis earum corrupti saepe rerum? Porro distinctio et corrupti? Velit nemo non a. Nulla explicabo molestias vitae accusamus veniam quaerat id nihil voluptate eveniet dolor exercitationem adipisci veritatis aliquam eum vero illo quo, similique excepturi tempore non. Nobis neque expedita itaque commodi, est quasi voluptatibus aspernatur impedit iste ad? Repellendus accusamus quidem eaque corrupti suscipit voluptatibus possimus. Hic, illum consequatur sint numquam eligendi, dolorem velit architecto, autem saepe delectus optio quasi et voluptatem eum dicta corporis sequi? Laboriosam consequuntur recusandae necessitatibus quibusdam, autem quisquam numquam minima aliquid dolorum vitae explicabo sapiente fuga? Enim, itaque iste sunt eaque laboriosam veritatis laudantium quisquam possimus blanditiis, voluptas, voluptatem libero architecto eius. Repellat facere ipsum id in maxime soluta suscipit cupiditate quia voluptas doloremque, iste laudantium tenetur nostrum beatae eveniet ea eligendi similique quis repellendus reiciendis necessitatibus quisquam. Voluptatum expedita excepturi nostrum natus corporis nemo aspernatur praesentium iure dignissimos non ipsum, itaque consequuntur veritatis porro, explicabo, et iste aliquam voluptas optio commodi dolorum. Vero corrupti iure iusto, fuga fugiat, recusandae illo suscipit dolorum, inventore ratione quidem reprehenderit veritatis laborum doloribus culpa et iste nulla nihil eligendi provident perferendis! Excepturi ad qui atque. Quod libero, corrupti unde culpa doloribus magni qui? Quas deserunt exercitationem adipisci? Maxime aliquid veritatis sed facilis doloremque repudiandae. Minima accusantium incidunt exercitationem ipsa. Voluptatibus modi voluptate molestiae asperiores sapiente earum vitae doloribus minima ipsum. Adipisci magni repellat culpa animi. Ullam illum maiores nihil fuga, at minus quam quo nam est hic. Odit quaerat nulla eaque soluta quas labore accusamus adipisci, debitis commodi fugiat doloremque et corporis corrupti quae, dolorem similique quibusdam perferendis temporibus omnis repudiandae cumque, quam est consectetur. Animi provident laboriosam ratione quaerat vero voluptates delectus, excepturi culpa consectetur rem reprehenderit fugiat odio modi ducimus nobis adipisci inventore laudantium sequi, pariatur eius quisquam at molestias. Quisquam aut corrupti voluptatem quis repudiandae enim autem dignissimos. Aperiam eveniet dolores laboriosam obcaecati natus eum veritatis, soluta deserunt labore alias! Dolorum, blanditiis officiis ad reiciendis similique porro architecto cupiditate praesentium at itaque molestiae hic alias ducimus quia error vitae magni assumenda perspiciatis incidunt voluptates, nisi omnis vel veniam. Facere nihil esse voluptates quo, ut consectetur, laudantium natus exercitationem consequuntur illo distinctio neque beatae aspernatur molestias asperiores id. Ducimus, deserunt? Sint rem maxime harum, dolor ipsam quas perferendis mollitia deleniti nam sequi sed corrupti molestias veritatis quaerat beatae sit, error voluptatum fugit delectus hic provident libero? Ducimus officiis dolor, facere, dolores voluptatum voluptatibus molestiae dicta non maxime in cumque! Sed obcaecati distinctio neque ut magnam. '.split(' ');
+const wordsCount = words.length;
+const gameTime = 40 * 1000;
+window.timer = null;
+window.gameStart = null;
+window.pauseTime = 0;
+
+function addClass(el,name) {
+  el.className += ' '+name;
+}
+function removeClass(el,name) {
+  el.className = el.className.replace(name,'');
+}
+
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordsCount);
+  return words[randomIndex - 1];
+}
+
+function formatWord(word) {
+  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+}
+
+function newGame() {
+  document.getElementById('words').innerHTML = '';
+  for (let i = 0; i < 200; i++) {
+    document.getElementById('words').innerHTML += formatWord(randomWord());
+  }
+  addClass(document.querySelector('.word'), 'current');
+  addClass(document.querySelector('.letter'), 'current');
+  document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+  window.timer = null;
+}
+
+function getWpm() {
+  const words = [...document.querySelectorAll('.word')];
+  const lastTypedWord = document.querySelector('.word.current');
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
+  const typedWords = words.slice(0, lastTypedWordIndex);
+  const correctWords = typedWords.filter(word => {
+    const letters = [...word.children];
+    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+    const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+    return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+  });
+  return correctWords.length / gameTime * 60000;
+}
+
+function gameOver() {
+  clearInterval(window.timer);
+  addClass(document.getElementById('game'), 'over');
+  const result = getWpm();
+  document.getElementById('info').innerHTML = `WPM: ${result}`;
+}
+
+document.getElementById('game').addEventListener('keyup', ev => {
+  const key = ev.key;
+  const currentWord = document.querySelector('.word.current');
+  const currentLetter = document.querySelector('.letter.current');
+  const expected = currentLetter?.innerHTML || ' ';
+  const isLetter = key.length === 1 && key !== ' ';
+  const isSpace = key === ' ';
+  const isBackspace = key === 'Backspace';
+  const isFirstLetter = currentLetter === currentWord.firstChild;
+
+  if (document.querySelector('#game.over')) {
+    return;
+  }
+
+  console.log({key,expected});
+
+  if (!window.timer && isLetter) {
+    window.timer = setInterval(() => {
+      if (!window.gameStart) {
+        window.gameStart = (new Date()).getTime();
+      }
+      const currentTime = (new Date()).getTime();
+      const msPassed = currentTime - window.gameStart;
+      const sPassed = Math.round(msPassed / 1000);
+      const sLeft = Math.round((gameTime / 1000) - sPassed);
+      if (sLeft <= 0) {
+        gameOver();
+        return;
+      }
+      document.getElementById('info').innerHTML = sLeft + '';
+    }, 1000);
+  }
+
+  if (isLetter) {
+    if (currentLetter) {
+      addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+      removeClass(currentLetter, 'current');
+      if (currentLetter.nextSibling) {
+        addClass(currentLetter.nextSibling, 'current');
+      }
+    } else {
+      const incorrectLetter = document.createElement('span');
+      incorrectLetter.innerHTML = key;
+      incorrectLetter.className = 'letter incorrect extra';
+      currentWord.appendChild(incorrectLetter);
+    }
+  }
+
+  if (isSpace) {
+    if (expected !== ' ') {
+      const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
+      lettersToInvalidate.forEach(letter => {
+        addClass(letter, 'incorrect');
+      });
+    }
+    removeClass(currentWord, 'current');
+    addClass(currentWord.nextSibling, 'current');
+    if (currentLetter) {
+      removeClass(currentLetter, 'current');
+    }
+    addClass(currentWord.nextSibling.firstChild, 'current');
+  }
+
+  if (isBackspace) {
+    if (currentLetter && isFirstLetter) {
+      // make prev word current, last letter current
+      removeClass(currentWord, 'current');
+      addClass(currentWord.previousSibling, 'current');
+      removeClass(currentLetter, 'current');
+      addClass(currentWord.previousSibling.lastChild, 'current');
+      removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+      removeClass(currentWord.previousSibling.lastChild, 'correct');
+    }
+    if (currentLetter && !isFirstLetter) {
+      // move back one letter, invalidate letter
+      removeClass(currentLetter, 'current');
+      addClass(currentLetter.previousSibling, 'current');
+      removeClass(currentLetter.previousSibling, 'incorrect');
+      removeClass(currentLetter.previousSibling, 'correct');
+    }
+    if (!currentLetter) {
+      addClass(currentWord.lastChild, 'current');
+      removeClass(currentWord.lastChild, 'incorrect');
+      removeClass(currentWord.lastChild, 'correct');
+    }
+  }
+
+  // move lines / words
+  if (currentWord.getBoundingClientRect().top > 250) {
+    const words = document.getElementById('words');
+    const margin = parseInt(words.style.marginTop || '0px');
+    words.style.marginTop = (margin - 35) + 'px';
+  }
+
+  // move cursor
+  const nextLetter = document.querySelector('.letter.current');
+  const nextWord = document.querySelector('.word.current');
+  const cursor = document.getElementById('cursor');
+  cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+  cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+});
+
+document.getElementById('newGameBtn').addEventListener('click', () => {
+  gameOver();
+  newGame();
+});
+
+newGame();
